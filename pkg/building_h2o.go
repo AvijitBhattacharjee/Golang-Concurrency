bd60011d74389b4db825abcb27ee7dc75fae51f0
Avijit Bhattacharjee <49098193+AvijitBhattacharjee@users.noreply.github.com>
2026-05-28 16:05:57 +0530

    add rateLimiter, building h20

--- pkg/building_h2o.go
+++ pkg/building_h2o.go
@@ -1,1 +1,112 @@
 package pkg
+
+import (
+	"fmt"
+	"sync"
+)
+
+type H2O struct {
+	oxygenChan   chan func()
+	hydrogenChan chan func()
+	done         chan struct{}
+}
+
+func NewH2O() *H2O {
+
+	h := &H2O{
+		oxygenChan:   make(chan func()),
+		hydrogenChan: make(chan func()),
+		done:         make(chan struct{}, 3),
+	}
+
+	go h.makeH2O()
+
+	return h
+}
+
+func (h *H2O) makeH2O() {
+
+	for {
+
+		// wait for exactly 2 Hydrogen
+		h1 := <-h.hydrogenChan
+		h2 := <-h.hydrogenChan
+
+		// wait for exactly 1 Oxygen
+		o := <-h.oxygenChan
+
+		// release atoms
+		h1()
+		h2()
+		o()
+
+		fmt.Println(" -> H2O formed")
+
+		// unblock waiting goroutines
+		h.done <- struct{}{}
+		h.done <- struct{}{}
+		h.done <- struct{}{}
+	}
+}
+
+func (h *H2O) Hydrogen(
+	releaseHydrogen func(),
+) {
+
+	h.hydrogenChan <- releaseHydrogen
+
+	<-h.done
+}
+
+func (h *H2O) Oxygen(
+	releaseOxygen func(),
+) {
+
+	h.oxygenChan <- releaseOxygen
+
+	<-h.done
+}
+
+func Build_H2O() {
+
+	h2o := NewH2O()
+
+	// 4H + 2O = 2 water molecules
+	input := "OOHHHH"
+
+	var wg sync.WaitGroup
+
+	wg.Add(len(input))
+
+	for _, ch := range input {
+
+		if ch == 'H' {
+
+			go func() {
+
+				defer wg.Done()
+
+				h2o.Hydrogen(func() {
+					fmt.Print("H")
+				})
+
+			}()
+
+		} else {
+
+			go func() {
+
+				defer wg.Done()
+
+				h2o.Oxygen(func() {
+					fmt.Print("O")
+				})
+
+			}()
+		}
+	}
+
+	wg.Wait()
+
+	fmt.Println("\nall molecules processed")
+}
--- pkg/rateLimiting.go
+++ pkg/rateLimiting.go
@@ -1,2 +1,59 @@
 package pkg
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type WorkLimiters struct {
+	numberOfWorker int
+	numberOfJobs   int
+	wg             sync.WaitGroup
+	jobs           chan int
+	limiter        <-chan time.Time
+}
+
+func RateLimiter() {
+
+	var WorkLimiter = WorkLimiters{
+		numberOfWorker: 3,
+		numberOfJobs:   10,
+		jobs:           make(chan int, 10),
+
+		// global rate limiter
+		// only 1 request allowed every 2 sec
+		limiter: time.Tick(2 * time.Second),
+		wg:      sync.WaitGroup{},
+	}
+
+	for i := 0; i < WorkLimiter.numberOfWorker; i++ {
+		WorkLimiter.wg.Add(1)
+		go WorkLimiter.workLimit(i)
+	}
+
+	WorkLimiter.receive()
+
+	close(WorkLimiter.jobs)
+	WorkLimiter.wg.Wait()
+	fmt.Println("all jobs are finished")
+}
+
+func (wl *WorkLimiters) workLimit(id int) {
+
+	defer wl.wg.Done()
+	for job := range wl.jobs {
+
+		// rate limiter blocks workers
+		<-wl.limiter
+		fmt.Printf("%d worker started = %d\n", id, job)
+		time.Sleep(2 * time.Second)
+		fmt.Printf("%d finished\n", id)
+	}
+}
+
+func (wl *WorkLimiters) receive() {
+	for i := 0; i < wl.numberOfJobs; i++ {
+		wl.jobs <- i
+	}
+}
